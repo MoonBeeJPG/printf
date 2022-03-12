c92ed5c78f581f84869c2d509a2f9239e7b40700
hellerdejanuar <hellerdejanuar@gmail.com>
2022-03-12 14:30:52 -0800

    pr_fn integer added

--- pr_fn.c
+++ pr_fn.c
@@ -13,6 +13,8 @@ int p_char(va_list list)
 
 	return (ch_c);
 }
+
+
 /**
 * p_str - Print the string function
 * Return: Number of characters printed
@@ -31,31 +33,88 @@ int p_str(va_list list)
 	}
 	return (ch_c);
 }
+
+
 /**
 * p_dec - Print the decimal function
-* Return: Number of characters printed
-*/
-int p_dec(va_list list)
-{
-    int ch_c = 0;
-
-    char dec = va_arg(list, int);
-    _putchar(dec);
-    ch_c++;
-
-    return (ch_c);
-}
-/**
-* p_int - Print the integral function
 * Return: Number of characters printed
 */
 int p_int(va_list list)
 {
     int ch_c = 0;
+    int num = va_arg(list, int);	
+	char *str;
 
-    char intg = va_arg(list, int);
-    _putchar(intg);
-    ch_c++;
+	str = itos(num);
+	
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+    	ch_c++;
+	}
+
+	free(str);
 
     return (ch_c);
+
+/* ------- UTILITIES ------ */
+
+/**
+ * itos - Converts int to string
+ * @num: number to convert to string
+ *
+ * Return: pointer to number string
+ */
+char *itos(int num)
+{
+	int digits, num_cp = num, rem, next_d;
+	char *n_str;
+
+	while (num_cp > 0)
+	{
+	num_cp /= 10;
+	digits++
+	}
+
+	n_str = malloc((digits + 1) * sizeof(char));
+
+	for (rem = num; rem != 0; rem /= 10)
+	{
+		next_d = rem % 10;
+		n_str[] = (next_d + '0');
+	}
+
+	n_str = rev_str(n_str);
+	return (n_str);
+}
+
+/**
+ * rev - reverses string (null char at the end)
+ * @s: string to be reversed
+ *
+ * Return: pointer to reversed string
+ */
+char *rev_str(char *str)
+{
+	int i, j = 0;
+	char *dest;
+	char *str;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+
+	dest = malloc(j * sizeof(char));
+	if (dest == NULL)
+		return (NULL);
+
+	j--;
+	
+	for (i = 0; str[i] != '\0'; i++, j--)
+	{
+		dest[i] = str[j];
+	}
+	desd[i] = '\0';
+	return (dest);
 }
