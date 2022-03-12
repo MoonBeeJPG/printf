6f6779cda2c97e7ca3b33563c704727be82dc37d
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-03-11 17:55:19 -0800

    main.h

--- main.h
+++ main.h
@@ -2,5 +2,13 @@
 #define MAIN_H
 int _printf(const char *format, ...);
 
+typedef struct comp
+{
+char id;
+void (*fn)(va_list list);
+}
+
+void p_char(va_list list);
+void p_str(va_list list);
 
 #endif
