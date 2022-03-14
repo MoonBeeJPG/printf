fb06053959dc87c56268ea5715f0e6c7a39e1670
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-03-14 04:57:24 -0700

    Try the checker

--- test/main.c
+++ test/main.c
@@ -9,10 +9,13 @@
  */
 int main(void)
 {
-    _printf("Character:[%c]\n", 'H');
+	int len;
+    int len2;
+    
+	_printf("Character:[%c]\n", 'H');
     printf("Character:[%c]\n", 'H');
     _printf("String:[%s]\n", "I am a string !");
     printf("String:[%s]\n", "I am a string !");
-    return (0);
+	return (0);	
 }
 
