8c8507251150234f230dfce78d6ff249aa24e82a
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-03-15 08:45:49 -0700

    Betty check

--- pr_fn.c
+++ pr_fn.c
@@ -38,12 +38,11 @@ int p_str(va_list list)
 	}
 	return (ch_c);
 }
-
 /**
- * p_mod - Print % as identifier
- * @list: list
- * Return: Number of characters printed
- */
+* p_mod - Print % as identifier
+* @list: list
+* Return: Number of characters printed
+*/
 int p_mod(va_list list)
 {
 	int ch_c = 0;
@@ -76,42 +75,43 @@ int p_unk(va_list list)
 */
 int p_bin(va_list list)
 {
-    char *value;
-    unsigned int var = 0, num = va_arg(list, int);
-    int ch_c = 0, count = 0;
+	char *value;
+	unsigned int var = 0, num = va_arg(list, int);
+	int ch_c = 0, count = 0;
 
-    if (num == 0)
-    {
-        _putchar(0);
-        return (1);
-    }
-    num = var;
+	if (num == 0)
+	{
+		_putchar(0);
+		return (1);
+	}
+	num = var;
 
-    while (var != 0)
-    {
-        while (var > 0)
-        {
-            var = var / 2;
-            ch_c++;
-        }
-        var = num;
-        value = malloc((sizeof(char) * ch_c) + 1);
-        if (!value)
-        {
-            while (var > 0)
-            {
-                value[count] = ((var % 2) + 48), ch_c++;
-                var = var / 2;
-            }
-            while (count != 0)
-            {
-                count--;
-                _putchar(value[count]);
-            }
-        }
-        else
-            return (-1);
-    }
-    free(value);
-    return (ch_c);
+	while (var != 0)
+	{
+		while (var > 0)
+		{
+			var = var / 2;
+			ch_c++;
+		}
+		var = num;
+		value = malloc((sizeof(char) * ch_c) + 1);
+
+		if (!value)
+		{
+			while (var > 0)
+			{
+			value[count] = ((var % 2) + 48), ch_c++;
+			var = var / 2;
+			}
+			while (count != 0)
+			{
+				count--;
+				_putchar(value[count]);
+			}
+		}
+		else
+			return (-1);
+	}
+	free(value);
+	return (ch_c);
 }
