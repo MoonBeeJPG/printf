220424d711dfd791342e476752d998b33cf69195
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-03-15 05:00:42 -0700

    bin

--- pr_bin.c
+++ pr_bin.c
@@ -4,51 +4,6 @@
  * @list: list
  * Return: Number the characters printed
  */
-int p_int(va_list list)
-{
-	int i = va_arg(list, int);
-	int cn = 0;
-	int dgts = 0, n, i_cp = i;
-
-	if (i < 0)
-	{
-		_putchar('-');
-		cn++;
-		i = 0 - i;
-	}
-	else if (i == 0)
-	{
-		_putchar('0');
-		return (1);
-	}
-
-	while (i_cp != 0)
-	{
-		i_cp /= 10;
-		dgts++;
-	}
-
-	dgts--;
-
-
-	for(; dgts > 0; dgts--)
-	{
-		n = i / _pwr(10, dgts);
-		_putchar (n + '0');
-		cn++;
-		i = i % _pwr(10, dgts);
-	}
-	
-	_putchar((i % 10) + '0');
-	cn++;
-
-	return (cn);
-}
-/**
-* p_bin - Print the binarian function
-* @list: list
-* Return: Number of characters printed
-*/
 int p_bin(va_list list)
 {
     char *value;
--- test/pr_int_rec.c
+++ test/pr_int_rec.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
  * pr_int_rec
  */
