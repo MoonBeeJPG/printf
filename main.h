976c058e7f590b73c8db45a4074ac50bb95c3722
hellerdejanuar <hellerdejanuar@gmail.com>
2022-03-15 06:22:28 -0700

    int working

--- main.h
+++ main.h
@@ -5,6 +5,7 @@
 #include <math.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <limits.h>
 int _printf(const char *format, ...);
 int _putchar(char c);
 
--- pr_nums.c
+++ pr_nums.c
@@ -64,3 +64,111 @@ int _pwr(int a, int b)
 		    else
 				        return (_pwr(a, b - 1)) * a;
 }
+
+/**
+ * pr_bin - Print a integer number
+ * @list: list
+ * Return: Number the characters printed
+ */
+int p_bin(va_list list)
+{
+    char *value;
+    unsigned int var = 0;
+	int ch_c = 0, count = 0;
+    unsigned int num = va_arg(list, int);
+
+    if (num == 0)
+    {
+        _putchar(0);
+        return (1);
+    }
+    num = var;
+
+    while (var != 0)
+    {
+        while (var > 0)
+        {
+            var = var / 2;
+            ch_c++;
+        }
+        var = num;
+        value = malloc((sizeof(char) * ch_c) +1);
+        if (!value)
+        {
+            while (var > 0)
+            {
+                value[count] = ((var % 2) + 48);
+                ch_c++;
+                var = var / 2;
+            }
+            while (count != 0)
+            {
+                count--;
+                _putchar(value[count]);
+            }
+        }
+        else
+            return (-1);
+    }
+    free(value);
+    return (ch_c);
+}
+
+/**
+* p_uns - Print unsigned int characters
+* @list: list
+* Return: Number of characters printed
+*/
+int p_uns(va_list list)
+{
+	char *value;
+    unsigned int var = 0;
+    int ch_c = 0, count = 0;
+    unsigned int num = va_arg(list, int);
+
+    while (num == 0)
+    {
+        _putchar(0);
+        return (1);
+    }
+    num = var;
+
+    while (var != 0)
+    {
+        while (var > 0)
+        {
+            var = var / 2;
+            ch_c++;
+        }
+        var = num;
+        value = malloc((sizeof(char) * ch_c) +1);
+        if (!value)
+        {
+            while (var > 0)
+            {
+                value[count] = ((var % 2) + 48);
+                ch_c++;
+                var = var / 2;
+            }
+            while (count != 0)
+            {
+                count--;
+                _putchar(value[count]);
+			}
+		}
+		else
+			return (-1);
+	}
+	free(value);
+	return (ch_c);
+}
+
+/**
+* p_oct - Print the octogonal character
+* @list: list
+* Return: Number of characters printed
+*/
+int p_oct(va_list list)
+{
+
+}
