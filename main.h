f2611c5182139568ca7eef37bdf06a5f6626b01b
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-03-15 05:49:27 -0700

    done

--- main.h
+++ main.h
@@ -25,6 +25,8 @@ int p_str(va_list list);
 int p_int(va_list list);
 int p_mod(va_list list);
 int p_unk(va_list list);
+int p_uns(va_list list);
+int p_oct(va_list list);
 int p_bin(va_list list);
 char *rev_str(char *);
 char *itos(int);
---
+++ pr_oct.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* p_oct - Print the octogonal character
+* @list: list
+* Return: Number of characters printed
+*/
+int p_oct(va_list list)
+{
+
+}
